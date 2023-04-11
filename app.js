488e57c0bb484f62239f65f4151ca2df56f90240
Rajesh <rajeshkumargupta_2k19mc102@dtu.ac.in>
2023-04-11 07:10:26 +0530

    8

--- app.js
+++ app.js
@@ -13,45 +13,51 @@
    
 
       var api= `https://newsapi.org/v2/everything?q=${search.value}&apiKey=b36f449f762a49498fd1940d3ec86847`;
+        
 
-    const response = await  fetch(api).catch(function (err){
-             console.log(err);
-             
-        });
+        try{
 
-    
-    const data = await response.json();
-    console.log(data);
+          const response = await fetch(api);
+          const data = await response.json();
+          console.log(data);
+      
+          // if(data){
+      
+          //     document.getElementById("cont").innerHTML="Something Went Wrong........";  
+          // }
 
-    if(data){
+          document.getElementById("cont").innerHTML ="";
+      
+           for(let i=0 ;i<= Math.min(10, data.articles.length);i++){
+      
+               var src= data.articles[i].urlToImage;
+               var title= data.articles[i].title;
+               var content=  data.articles[i].description;
+      
+                console.log(src);
+                console.log("hello");
+               var divele= ` <div class="card mb-3 mt-3">
+      <img src= ${src} class="card-img-top" alt="...", width="50%">
+      <div class="card-body">
+        <h5 class="card-title">${title}</h5>
+        <p class="card-text"> ${content}</p>
+        
+      </div>
+      </div>`
+      
+        console.log(divele);
+      
+         document.getElementById("cont").innerHTML+= divele;
+      
+           }
 
-        document.getElementById("cont").innerHTML="Something Went Wrong........";  
-    }
+        }catch{
 
-     for(let i=0 ;i<= Math.min(10, data.articles.length);i++){
-
-         var src= data.articles[i].urlToImage;
-         var title= data.articles[i].title;
-         var content=  data.articles[i].description;
-
-          console.log(src);
-          console.log("hello");
-         var divele= ` <div class="card mb-3 mt-3">
-<img src= ${src} class="card-img-top" alt="...", width="50%">
-<div class="card-body">
-  <h5 class="card-title">${title}</h5>
-  <p class="card-text"> ${content}</p>
-  
-</div>
-</div>`
-
-  console.log(divele);
-
-   document.getElementById("cont").innerHTML+= divele;
-
-
+          document.getElementById("cont").innerHTML="Something Went Wrong.......?????"  
+        }
+        
      }
 
 
      
- }
+ 
